ab4ff6897e044cb49e434b65dc20e3852c84941b
David Carr <david.carr@digital.cabinet-office.gov.uk>
2024-07-10 16:02:22 +0100

    Tests

--- feature/onboarding/src/main/kotlin/uk/govuk/app/onboarding/OnboardingViewModel.kt
+++ feature/onboarding/src/main/kotlin/uk/govuk/app/onboarding/OnboardingViewModel.kt
@@ -19,24 +19,35 @@ class OnboardingViewModel @Inject constructor(
     private val onboardingAnalytics: OnboardingAnalytics
 ): ViewModel() {
 
+    companion object {
+        private const val ONBOARDING_SCREEN_1_ALIAS = "ONBOARDING_A"
+        private const val ONBOARDING_SCREEN_2_ALIAS = "ONBOARDING_B"
+        private const val ONBOARDING_SCREEN_3_ALIAS = "ONBOARDING_C"
+
+        private const val SCREEN_CLASS = "OnboardingScreen"
+        private const val CONTINUE_ACTION = "continue"
+        private const val SKIP_ACTION = "skip"
+        private const val DONE_ACTION = "done"
+    }
+
     // Todo - this will probably come from JSON file or remote config etc
     private val _uiState: MutableStateFlow<OnboardingUiState> = MutableStateFlow(
         OnboardingUiState(
             listOf(
                 OnboardingPage(
-                    analyticsAlias = "ONBOARDING_A",
+                    analyticsAlias = ONBOARDING_SCREEN_1_ALIAS,
                     title = R.string.getThingsDoneScreenTitle,
                     body = R.string.getThingsDoneScreenBody,
                     image = R.drawable.image_1
                 ),
                 OnboardingPage(
-                    analyticsAlias = "ONBOARDING_B",
+                    analyticsAlias = ONBOARDING_SCREEN_2_ALIAS,
                     title = R.string.backToPreviousScreenTitle,
                     body = R.string.backToPreviousScreenBody,
                     image = R.drawable.image_2
                 ),
                 OnboardingPage(
-                    analyticsAlias = "ONBOARDING_C",
+                    analyticsAlias = ONBOARDING_SCREEN_3_ALIAS,
                     title = R.string.tailoredToYouScreenTitle,
                     body = R.string.tailoredToYouScreenBody,
                     image = R.drawable.image_3
@@ -49,7 +60,7 @@ class OnboardingViewModel @Inject constructor(
     fun onPageView(pageIndex: Int) {
         val page = uiState.value.pages[pageIndex]
         onboardingAnalytics.onboardingScreenView(
-            screenClass = "OnboardingScreen",
+            screenClass = SCREEN_CLASS,
             alias = page.analyticsAlias,
             title = context.getString(page.title)
         )
@@ -59,7 +70,7 @@ class OnboardingViewModel @Inject constructor(
         logButtonClick(
             pageIndex = pageIndex,
             cta = cta,
-            action = "continue"
+            action = CONTINUE_ACTION
         )
     }
 
@@ -67,7 +78,7 @@ class OnboardingViewModel @Inject constructor(
         logButtonClick(
             pageIndex = pageIndex,
             cta = cta,
-            action = "skip"
+            action = SKIP_ACTION
         )
     }
 
@@ -75,7 +86,7 @@ class OnboardingViewModel @Inject constructor(
         logButtonClick(
             pageIndex = pageIndex,
             cta = cta,
-            action = "done"
+            action = DONE_ACTION
         )
     }
 
