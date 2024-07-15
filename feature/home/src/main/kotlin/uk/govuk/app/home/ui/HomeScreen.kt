32e88494f6e7390605dcbf6d65f2504e593e049c
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2024-07-15 15:45:36 +0100

    Add collapsable header and scrolling content for the homepage

--- feature/home/src/main/kotlin/uk/govuk/app/home/ui/HomeScreen.kt
+++ feature/home/src/main/kotlin/uk/govuk/app/home/ui/HomeScreen.kt
@@ -1,7 +1,42 @@
 package uk.govuk.app.home.ui
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 internal fun HomeRoute() {
@@ -9,7 +44,113 @@ internal fun HomeRoute() {
     HomeScreen()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun HomeScreen() {
-    Text("Home Screen")
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+    val isCollapsed = remember { derivedStateOf { scrollBehavior.state.collapsedFraction > 0.5 } }
+
+    val headerHeight by animateDpAsState(
+        targetValue = if (isCollapsed.value) 80.dp else 135.dp
+    )
+
+    val fontSize = if (isCollapsed.value) 22.sp else 28.sp
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        topBar = {
+            LargeTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFFFAFAFA),
+                    scrolledContainerColor = Color(0xFFFAFAFA),
+                ),
+                title = {
+                    Box(
+                        modifier = Modifier.fillMaxSize()
+                            .wrapContentSize()
+                    ) {
+                        Text(
+                            text = govUkText(fontSize),
+                            maxLines = 1,
+                            textAlign = TextAlign.Center,
+                            overflow = TextOverflow.Ellipsis,
+                        )
+                    }
+                },
+                modifier = Modifier.height(headerHeight),
+                scrollBehavior = scrollBehavior,
+            )
+        },
+        content = { innerPadding ->
+            LazyColumn(
+                contentPadding = innerPadding,
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.background(Color(0xFFFAFAFA))
+            ) {
+                val list = (1..12).map { it.toString() }
+                items(count = list.size) {
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color(0xFFFFFFFF),
+                        ),
+                        border = BorderStroke(1.dp, Color(0xFFB1B4B6)),
+                        modifier = Modifier
+                            .padding(16.dp, 12.dp, 16.dp, 0.dp)
+                            .fillMaxSize()
+                            .height(200.dp)
+                            .background(Color(0xFFB1B4B6), shape = RoundedCornerShape(10.dp))
+                    ) {
+                        Text(
+                            text = "Scrollable content",
+                            style = MaterialTheme.typography.bodyMedium,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
+                        )
+                    }
+                }
+            }
+        }
+    )
+}
+
+fun govUkText(fontSize: androidx.compose.ui.unit.TextUnit): AnnotatedString {
+    val textColor = Color(0xFF195AAA)
+    val dotColor = Color(0xFF17C7FF)
+    val fontWeight = FontWeight.Bold
+
+    return buildAnnotatedString {
+        withStyle(
+            style = SpanStyle(
+                fontWeight = fontWeight,
+                fontSize = fontSize,
+                color = textColor,
+                baselineShift = BaselineShift(multiplier = 1f)
+            )
+        ) {
+            append("GOV")
+        }
+        withStyle(
+            style = SpanStyle(
+                fontWeight = fontWeight,
+                fontSize = ((fontSize.value / 2) + fontSize.value).sp,
+                color = dotColor,
+                baselineShift = BaselineShift(multiplier = 1.3f)
+            )
+        ) {
+            append(".")
+        }
+        withStyle(
+            style = SpanStyle(
+                fontWeight = fontWeight,
+                fontSize = fontSize,
+                color = textColor,
+                baselineShift = BaselineShift(multiplier = 1f)
+            )
+        ) {
+            append("UK")
+        }
+    }
 }
