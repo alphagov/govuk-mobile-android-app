d8f69562acf61a15d1a020fe88038294d784e423
David Carr <david.carr@digital.cabinet-office.gov.uk>
2024-07-16 14:27:45 +0100

    Replace deprecated code

--- feature/onboarding/src/main/kotlin/uk/govuk/app/onboarding/ui/OnboardingScreen.kt
+++ feature/onboarding/src/main/kotlin/uk/govuk/app/onboarding/ui/OnboardingScreen.kt
@@ -24,7 +24,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
@@ -115,8 +115,7 @@ private fun OnboardingScreen(
             Page(pages[pageIndex])
         }
 
-        Divider(
-            modifier = Modifier.fillMaxWidth(),
+        HorizontalDivider(
             thickness = 1.dp,
             color = GovUkTheme.colourScheme.strokes.listDivider,
         )
