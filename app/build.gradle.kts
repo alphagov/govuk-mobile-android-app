4f0a57b9762ba6e955dfd21abdee9e9e55c5a845
David Carr <david.carr@digital.cabinet-office.gov.uk>
2024-07-16 13:52:20 +0100

    Remove unused dependancies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,6 @@ dependencies {
 
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.hilt.android)
