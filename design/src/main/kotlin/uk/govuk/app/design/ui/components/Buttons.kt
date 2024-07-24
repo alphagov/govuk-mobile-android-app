5902b01bed5f230e198dbb0782c028a96f97b3d7
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2024-07-24 14:42:49 +0100

    Refactor primary and secondary buttons from the onboarding module into the design module

---
+++ design/src/main/kotlin/uk/govuk/app/design/ui/components/Buttons.kt
@@ -0,0 +1,48 @@
+package uk.govuk.app.design.ui.components
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import uk.govuk.app.design.ui.theme.GovUkTheme
+
+@Composable
+fun PrimaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = GovUkTheme.colourScheme.surfaces.buttonPrimary,
+            contentColor = GovUkTheme.colourScheme.textAndIcons.buttonPrimary
+        )
+    ) {
+        Text(
+            text = text,
+            style = GovUkTheme.typography.bodyBold,
+        )
+    }
+}
+
+@Composable
+fun SecondaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Text(
+            text = text,
+            color = GovUkTheme.colourScheme.textAndIcons.link,
+            style = GovUkTheme.typography.bodyRegular,
+        )
+    }
+}
