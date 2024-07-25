dca535f6c8c1f754161898828688a2a1aa8c87d8
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2024-07-25 15:12:22 +0100

    Add buttons and tests

--- design/src/androidTest/kotlin/uk/govuk/app/design/ui/components/ButtonsTest.kt
+++ design/src/androidTest/kotlin/uk/govuk/app/design/ui/components/ButtonsTest.kt
@@ -1,10 +1,11 @@
 package uk.govuk.app.design.ui.components
 
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
@@ -12,20 +13,40 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ButtonsTest {
+    private val buttonText = "Hello, World!"
+
     @get:Rule
     val composeTestRule = createComposeRule()
 
     @Test
-    fun buttonClickShouldUpdateTheButtonText() {
-        composeTestRule.setContent { TestButton {} }
-        composeTestRule.onNodeWithText("Not Clicked").performClick()
-        composeTestRule.onNodeWithText("Clicked").assertIsDisplayed()
+    fun canCreateAnEnabledPrimaryButton() {
+        composeTestRule.setContent { PrimaryButton(buttonText, {}, modifier = Modifier) }
+        val button = composeTestRule.onNodeWithText(buttonText)
+        button.assertIsDisplayed()
+        button.assertIsEnabled()
     }
 
     @Test
-    fun buttonClickShouldNotRemainTheSame() {
-        composeTestRule.setContent { TestButton {} }
-        composeTestRule.onNodeWithText("Not Clicked").performClick()
-        composeTestRule.onNodeWithText("Not Clicked").assertIsNotDisplayed()
+    fun canCreateADisabledPrimaryButton() {
+        composeTestRule.setContent { PrimaryButton(buttonText, {}, modifier = Modifier, enabled = false) }
+        val button = composeTestRule.onNodeWithText(buttonText)
+        button.assertIsDisplayed()
+        button.assertIsNotEnabled()
+    }
+
+    @Test
+    fun canCreateAnEnabledSecondaryButton() {
+        composeTestRule.setContent { SecondaryButton(buttonText, {}, modifier = Modifier) }
+        val button = composeTestRule.onNodeWithText(buttonText)
+        button.assertIsDisplayed()
+        button.assertIsEnabled()
+    }
+
+    @Test
+    fun canCreateADisabledSecondaryButton() {
+        composeTestRule.setContent { SecondaryButton(buttonText, {}, modifier = Modifier, enabled = false) }
+        val button = composeTestRule.onNodeWithText(buttonText)
+        button.assertIsDisplayed()
+        button.assertIsNotEnabled()
     }
 }
--- design/src/main/kotlin/uk/govuk/app/design/ui/components/Buttons.kt
+++ design/src/main/kotlin/uk/govuk/app/design/ui/components/Buttons.kt
@@ -5,33 +5,19 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import uk.govuk.app.design.ui.theme.GovUkTheme
-
-@Composable
-fun TestButton(onClick: () -> Unit) {
-    var buttonClicked by remember { mutableStateOf(false) }
-
-    Button(onClick = {
-        buttonClicked = true
-        onClick()
-    }) {
-        Text(if (buttonClicked) "Clicked" else "Not Clicked")
-    }
-}
 
 @Composable
 fun PrimaryButton(
     text: String,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
 ) {
     Button(
         onClick = onClick,
+        enabled = enabled,
         modifier = modifier,
         colors = ButtonDefaults.buttonColors(
             containerColor = GovUkTheme.colourScheme.surfaces.buttonPrimary,
@@ -49,15 +35,17 @@ fun PrimaryButton(
 fun SecondaryButton(
     text: String,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
 ) {
     TextButton(
         onClick = onClick,
+        enabled = enabled,
         modifier = modifier
     ) {
         Text(
             text = text,
-            color = GovUkTheme.colourScheme.textAndIcons.link,
+            color = GovUkTheme.colourScheme.textAndIcons.buttonSecondary,
             style = GovUkTheme.typography.bodyRegular,
         )
     }
