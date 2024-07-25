9c3f28ed51f004fdc17269277bc373656c3411de
David Carr <david.carr@digital.cabinet-office.gov.uk>
2024-07-25 14:15:11 +0100

    Upgrade sonar version, add source property, move sonar block to root build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,21 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.google.play.services) apply false
     alias(libs.plugins.crashlytics) apply false
+    id("org.sonarqube") version "5.0.0.4638"
+}
+
+subprojects {
+    sonar {
+        properties {
+            property("sonar.projectName", "govuk-mobile-android-app")
+            property("sonar.projectKey", "alphagov_govuk-mobile-android-app")
+            property("sonar.organization", "alphagov")
+            property("sonar.host.url", "https://sonarcloud.io")
+            property("sonar.sourceEncoding", "UTF-8")
+            property("sonar.sources", "src/main")
+            property("sonar.qualitygate.wait", "true")
+            property("sonar.androidLint.reportPaths", "${projectDir}/build/reports/lint-results-debug.xml")
+//        property("sonar.coverage.jacoco.xmlReportPaths", "${projectDir}/build/test-results/testDebugUnitTest/TEST-*.xml")
+        }
+    }
 }
