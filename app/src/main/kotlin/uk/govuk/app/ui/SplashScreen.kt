90c47f45fc434a8a59dbbddd13c1fb3b7a57a33e
David Carr <david.carr@digital.cabinet-office.gov.uk>
2024-08-13 16:04:58 +0100

    Separate splash screen into it's own activity and bypass for deep links

---
+++ app/src/main/kotlin/uk/govuk/app/ui/SplashScreen.kt
@@ -0,0 +1,67 @@
+package uk.govuk.app.ui
+
+import android.content.Context
+import android.provider.Settings
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import kotlinx.coroutines.delay
+import uk.govuk.app.R
+
+@Composable
+fun SplashScreen(
+    onSplashDone: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val composition by rememberLottieComposition(
+            LottieCompositionSpec.RawRes(R.raw.app_splash)
+        )
+
+        var state = animateLottieCompositionAsState(composition = composition)
+
+        // Handle cases where animation is disabled...
+        if (areAnimationsDisabled(LocalContext.current)) {
+            state = animateLottieCompositionAsState(composition = composition, isPlaying = false)
+            LaunchedEffect(true) {
+                delay(6000) // wait for 6 seconds
+                onSplashDone()
+            }
+            // Animations are enabled...
+        } else {
+            LaunchedEffect(state.progress) {
+                if (state.progress == 1f) {
+                    onSplashDone()
+                }
+            }
+        }
+
+        LottieAnimation(
+            composition = composition,
+            progress = { state.progress }
+        )
+    }
+}
+
+fun areAnimationsDisabled(context: Context): Boolean {
+    val animatorDurationScale = Settings.Global.getFloat(
+        context.contentResolver,
+        Settings.Global.ANIMATOR_DURATION_SCALE,
+        1f
+    )
+    return animatorDurationScale == 0f
+}
