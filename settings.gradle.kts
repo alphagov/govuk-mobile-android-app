da21c001dc9f918771d9a994321a6b596206bd5e
David Carr <david.carr@digital.cabinet-office.gov.uk>
2024-08-16 10:40:10 +0100

    Order module includes alphabetically

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -41,10 +41,10 @@ dependencyResolutionManagement {
 rootProject.name = "govuk-mobile-android-app"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-include(":app")
 include(":analytics")
+include(":app")
 include(":design")
 include(":feature:home")
-include(":feature:settings")
 include(":feature:onboarding")
 include(":feature:search")
+include(":feature:settings")
