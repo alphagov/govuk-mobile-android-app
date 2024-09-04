75bc2c65d4ccb766260a9488766c03cca10518c4
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2024-09-04 15:50:39 +0100

    Make settings list scrollable

--- feature/settings/src/main/kotlin/uk/govuk/app/settings/ui/SettingsScreen.kt
+++ feature/settings/src/main/kotlin/uk/govuk/app/settings/ui/SettingsScreen.kt
@@ -3,8 +3,12 @@ package uk.govuk.app.settings.ui
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedCard
@@ -49,10 +53,15 @@ private fun SettingsScreen(
         onPageView()
     }
 
-    Column(modifier) {
+    Column {
         BaseHeader("Settings")
-        AboutTheApp(onButtonClick)
-        PrivacyAndLegal()
+        Column(
+            modifier = Modifier.verticalScroll(rememberScrollState())
+        ) {
+            AboutTheApp(onButtonClick)
+            PrivacyAndLegal()
+            Spacer(Modifier.height(100.dp))
+        }
     }
 }
 
