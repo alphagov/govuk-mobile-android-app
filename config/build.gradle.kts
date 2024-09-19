1e2af1273832505d7850af42ff2e18b3ff6796a3
David Carr <david.carr@digital.cabinet-office.gov.uk>
2024-09-19 10:52:47 +0100

    Remove redundant build dependancies

--- config/build.gradle.kts
+++ config/build.gradle.kts
@@ -25,9 +25,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.material3)
     implementation(libs.hilt.android)
 
     ksp(libs.hilt.compiler)
