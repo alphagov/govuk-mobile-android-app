f683aefa1adc7b8121f437410a45fa37fa118d3d
David Carr <david.carr@digital.cabinet-office.gov.uk>
2024-09-27 15:03:30 +0100

    Attempt to display topics - getting a crash

--- feature/topics/src/main/kotlin/uk/govuk/app/topics/TopicsViewModel.kt
+++ feature/topics/src/main/kotlin/uk/govuk/app/topics/TopicsViewModel.kt
@@ -1,21 +1,34 @@
 package uk.govuk.app.topics
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import uk.govuk.app.topics.data.remote.model.TopicItem
 import javax.inject.Inject
+
+internal data class TopicsUiState(
+    // Todo - should probably have loading and error states
+    val topics: List<TopicItem>
+)
 
 @HiltViewModel
 internal class TopicsViewModel @Inject constructor(
     private val topicsRepo: TopicsRepo
 ): ViewModel() {
 
+    private val _uiState: MutableStateFlow<TopicsUiState?> = MutableStateFlow(null)
+    val uiState = _uiState.asStateFlow()
+
     init {
         viewModelScope.launch {
             val topics = topicsRepo.getTopics()
-            Log.d("Topics", "$topics")
+            // Todo - loading and error states etc
+            topics?.let {
+                _uiState.value = TopicsUiState(it)
+            }
         }
     }
 
--- feature/topics/src/main/kotlin/uk/govuk/app/topics/ui/widget/TopicsWidget.kt
+++ feature/topics/src/main/kotlin/uk/govuk/app/topics/ui/widget/TopicsWidget.kt
@@ -6,10 +6,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,24 +27,31 @@ fun TopicsWidget(
     modifier: Modifier = Modifier
 ) {
     val viewModel: TopicsViewModel = hiltViewModel()
+    val uiState by viewModel.uiState.collectAsState()
 
     Column(modifier = modifier) {
         Title3BoldLabel("Topics")
 
-        OutlinedCard(
-            colors = CardDefaults.cardColors(
-                containerColor = GovUkTheme.colourScheme.surfaces.card
-            ),
-            border = BorderStroke(
-                width = 1.dp,
-                color = GovUkTheme.colourScheme.strokes.listDivider
-            )
-        ) {
-            Box(
-                Modifier
-                    .padding(GovUkTheme.spacing.medium)
-            ){
-                Text("Topic name")
+        uiState?.topics?.let{ topics ->
+            LazyColumn {
+                items(topics) { topic ->
+                    OutlinedCard(
+                        colors = CardDefaults.cardColors(
+                            containerColor = GovUkTheme.colourScheme.surfaces.card
+                        ),
+                        border = BorderStroke(
+                            width = 1.dp,
+                            color = GovUkTheme.colourScheme.strokes.listDivider
+                        )
+                    ) {
+                        Box(
+                            Modifier
+                                .padding(GovUkTheme.spacing.medium)
+                        ){
+                            Text(topic.title)
+                        }
+                    }
+                }
             }
         }
     }
