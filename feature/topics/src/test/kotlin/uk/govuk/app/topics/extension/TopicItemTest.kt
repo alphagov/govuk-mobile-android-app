b16ca29a7c197a40f386b7f3350e68dfe0f4ef44
David Carr <david.carr@digital.cabinet-office.gov.uk>
2024-10-02 14:50:37 +0100

    Simplify topic mapping tests

--- feature/topics/src/test/kotlin/uk/govuk/app/topics/extension/TopicItemTest.kt
+++ feature/topics/src/test/kotlin/uk/govuk/app/topics/extension/TopicItemTest.kt
@@ -9,206 +9,30 @@ import uk.govuk.app.topics.data.remote.model.TopicItem
 class TopicItemTest{
 
     @Test
-    fun `Given a benefits topic item, When mapping to topic ui, then return a topic ui with correct icon`() {
-        val expected = TopicUi(
-            ref = "benefits",
-            title = "Title",
-            icon = R.drawable.ic_topic_benefits
+    fun `Given topic items, When mapping to topic ui, then return a topic ui with correct icon`() {
+        val topicItems = listOf(
+            Pair(R.drawable.ic_topic_benefits, TopicItem("benefits", "Title")),
+            Pair(R.drawable.ic_topic_business, TopicItem("business", "Title")),
+            Pair(R.drawable.ic_topic_care, TopicItem("care", "Title")),
+            Pair(R.drawable.ic_topic_transport, TopicItem("driving-transport", "Title")),
+            Pair(R.drawable.ic_topic_employment, TopicItem("employment", "Title")),
+            Pair(R.drawable.ic_topic_health, TopicItem("health-disability", "Title")),
+            Pair(R.drawable.ic_topic_money, TopicItem("money-tax", "Title")),
+            Pair(R.drawable.ic_topic_parenting, TopicItem("parenting-guardianship", "Title")),
+            Pair(R.drawable.ic_topic_retirement, TopicItem("retirement", "Title")),
+            Pair(R.drawable.ic_topic_studying, TopicItem("studying-training", "Title")),
+            Pair(R.drawable.ic_topic_travel, TopicItem("travel", "Title")),
+            Pair(R.drawable.ic_topic_default, TopicItem("unknown", "Title")),
         )
 
-        val actual =
-            TopicItem(
-                ref = "benefits",
-                title = "Title"
-            ).toTopicUi()
+        for (topicItem in topicItems) {
+            val expected = TopicUi(
+                ref = topicItem.second.ref,
+                icon = topicItem.first,
+                title = topicItem.second.title
+            )
 
-        assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `Given a business topic item, When mapping to topic ui, then return a a topic ui with correct icon`() {
-        val expected = TopicUi(
-            ref = "business",
-            title = "Title",
-            icon = R.drawable.ic_topic_business
-        )
-
-        val actual =
-            TopicItem(
-                ref = "business",
-                title = "Title"
-            ).toTopicUi()
-
-        assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `Given a care topic item, When mapping to topic ui, then return a a topic ui with correct icon`() {
-        val expected = TopicUi(
-            ref = "care",
-            title = "Title",
-            icon = R.drawable.ic_topic_care
-        )
-
-        val actual =
-            TopicItem(
-                ref = "care",
-                title = "Title"
-            ).toTopicUi()
-
-        assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `Given a transport topic item, When mapping to topic ui, then return a a topic ui with correct icon`() {
-        val expected = TopicUi(
-            ref = "driving-transport",
-            title = "Title",
-            icon = R.drawable.ic_topic_transport
-        )
-
-        val actual =
-            TopicItem(
-                ref = "driving-transport",
-                title = "Title"
-            ).toTopicUi()
-
-        assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `Given an employment topic item, When mapping to topic ui, then return a a topic ui with correct icon`() {
-        val expected = TopicUi(
-            ref = "employment",
-            title = "Title",
-            icon = R.drawable.ic_topic_employment
-        )
-
-        val actual =
-            TopicItem(
-                ref = "employment",
-                title = "Title"
-            ).toTopicUi()
-
-        assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `Given a health topic item, When mapping to topic ui, then return a a topic ui with correct icon`() {
-        val expected = TopicUi(
-            ref = "health-disability",
-            title = "Title",
-            icon = R.drawable.ic_topic_health
-        )
-
-        val actual =
-            TopicItem(
-                ref = "health-disability",
-                title = "Title"
-            ).toTopicUi()
-
-        assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `Given a money topic item, When mapping to topic ui, then return a a topic ui with correct icon`() {
-        val expected = TopicUi(
-            ref = "money-tax",
-            title = "Title",
-            icon = R.drawable.ic_topic_money
-        )
-
-        val actual =
-            TopicItem(
-                ref = "money-tax",
-                title = "Title"
-            ).toTopicUi()
-
-        assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `Given a parenting topic item, When mapping to topic ui, then return a a topic ui with correct icon`() {
-        val expected = TopicUi(
-            ref = "parenting-guardianship",
-            title = "Title",
-            icon = R.drawable.ic_topic_parenting
-        )
-
-        val actual =
-            TopicItem(
-                ref = "parenting-guardianship",
-                title = "Title"
-            ).toTopicUi()
-
-        assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `Given a retirement topic item, When mapping to topic ui, then return a a topic ui with correct icon`() {
-        val expected = TopicUi(
-            ref = "retirement",
-            title = "Title",
-            icon = R.drawable.ic_topic_retirement
-        )
-
-        val actual =
-            TopicItem(
-                ref = "retirement",
-                title = "Title"
-            ).toTopicUi()
-
-        assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `Given a studying topic item, When mapping to topic ui, then return a a topic ui with correct icon`() {
-        val expected = TopicUi(
-            ref = "studying-training",
-            title = "Title",
-            icon = R.drawable.ic_topic_studying
-        )
-
-        val actual =
-            TopicItem(
-                ref = "studying-training",
-                title = "Title"
-            ).toTopicUi()
-
-        assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `Given a travel topic item, When mapping to topic ui, then return a a topic ui with correct icon`() {
-        val expected = TopicUi(
-            ref = "travel",
-            title = "Title",
-            icon = R.drawable.ic_topic_travel
-        )
-
-        val actual =
-            TopicItem(
-                ref = "travel",
-                title = "Title"
-            ).toTopicUi()
-
-        assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `Given an unknown topic item, When mapping to topic ui, then return a a topic ui with correct icon`() {
-        val expected = TopicUi(
-            ref = "unknown",
-            title = "Title",
-            icon = R.drawable.ic_topic_default
-        )
-
-        val actual =
-            TopicItem(
-                ref = "unknown",
-                title = "Title"
-            ).toTopicUi()
-
-        assertEquals(actual, expected)
+            assertEquals(topicItem.second.toTopicUi(), expected)
+        }
     }
 }
