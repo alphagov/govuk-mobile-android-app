fbcd2b6c5026f41ddab84efe0b333b57f4fc6000
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2024-10-22 11:26:30 +0100

    Fix broken tests

--- feature/visited/src/test/kotlin/uk/govuk/app/visited/VisitedViewModelTest.kt
+++ feature/visited/src/test/kotlin/uk/govuk/app/visited/VisitedViewModelTest.kt
@@ -1,91 +1,121 @@
 package uk.govuk.app.visited
 
 import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
-import org.junit.experimental.runners.Enclosed
-import org.junit.runner.RunWith
 import uk.govuk.app.analytics.Analytics
+import uk.govuk.app.visited.data.VisitedRepo
+import uk.govuk.app.visited.data.model.VisitedItem
+import uk.govuk.app.visited.ui.model.VisitedUi
 
-@RunWith(Enclosed::class)
 @OptIn(ExperimentalCoroutinesApi::class)
 class VisitedViewModelTest {
-    class AnalyticsTest {
-        private val analytics = mockk<Analytics>(relaxed = true)
-        private val viewModel = VisitedViewModel(analytics)
-        private val dispatcher = UnconfinedTestDispatcher()
+    private val dispatcher = UnconfinedTestDispatcher()
+    private val visitedRepo = mockk<VisitedRepo>(relaxed = true)
+    private val analytics = mockk<Analytics>(relaxed = true)
+    private val viewModel = VisitedViewModel(visitedRepo, analytics)
 
-        @Test
-        fun `Given a page view, then log analytics`() {
-            viewModel.onPageView()
+    @Before
+    fun setup() {
+        Dispatchers.setMain(dispatcher)
+    }
 
-            verify {
-                analytics.screenView(
-                    screenClass = "VisitedScreen",
-                    screenName = "Pages you've visited",
-                    title = "Pages you've visited"
-                )
-            }
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `Given a page view, then log analytics`() {
+        viewModel.onPageView()
+
+        verify {
+            analytics.screenView(
+                screenClass = "VisitedScreen",
+                screenName = "Pages you've visited",
+                title = "Pages you've visited"
+            )
         }
+    }
 
-        @Test
-        fun `Given the user views the visited items screen, and a visited item is clicked, then log analytics`() {
-            Dispatchers.setMain(dispatcher)
+    @Test
+    fun `Given the user views the visited items screen, and a visited item is clicked, then log analytics`() {
+        viewModel.onVisitedItemClicked("visited item title", "visited item title")
 
-            viewModel.onVisitedItemClicked("visited item title", "visited item title")
-
-            runTest {
-                coVerify {
-                    analytics.visitedItemClick("visited item title", "visited item title")
-                }
+        runTest {
+            coVerify {
+                analytics.visitedItemClick("visited item title", "visited item title")
             }
         }
     }
 
-    class UiStateTest {
-        private val analytics = mockk<Analytics>(relaxed = true)
-        private val viewModel = VisitedViewModel(analytics)
+    @Test
+    fun `Given there are visited items, then the status in the view model is correct`() {
+        val visitedItems = listOf(
+            VisitedItem().apply {
+                title = "GOV.UK"
+                url = "https://www.gov.uk"
+                lastVisited = "24 July 2024"
+            },
+            VisitedItem().apply {
+                title = "Google"
+                url = "https://www.google.com"
+                lastVisited = "24 July 2024"
+            }
+        )
 
-        @Test
-        fun `Given there is a visited item, then the status in the view model is correct`() {
-            val visitedItems = VisitedUiState(
+        val expected =
+            VisitedUiState(
                 visited = listOf(
-                    VisitedUi("GOV.UK", "https://www.gov.uk", "24 July 2024")
+                    VisitedUi(
+                        title = "GOV.UK",
+                        url = "https://www.gov.uk",
+                        lastVisited = "24 July 2024"
+                    ),
+                    VisitedUi(
+                        title = "Google",
+                        url = "https://www.google.com",
+                        lastVisited = "24 July 2024"
+                    )
                 )
             )
 
-            /* TODO...
-            coEvery { /* mock where the visited items are retrieved from */ } returns visitedItems
-            */
+        val viewModel = VisitedViewModel(visitedRepo, analytics)
 
-            viewModel.onPageView()
+        every { visitedRepo.visitedItems } returns flowOf(visitedItems)
 
-            runTest {
-                val result = viewModel.uiState.first()
+        viewModel.onPageView()
 
-                /* TODO: assertEquals(visitedItems, result) */
-            }
+        runTest {
+            assertEquals(expected, viewModel.uiState.first())
         }
+    }
 
-        @Test
-        fun `Given there are no visited items, then the status in the view model are correct`() {
-            val visitedItems = VisitedUiState(visited = emptyList())
+    @Test
+    fun `Given there are no visited items, then the status in the view model are correct`() {
+        val visitedItems = emptyList<VisitedItem>()
+        val expected = VisitedUiState(visited = emptyList())
+        val viewModel = VisitedViewModel(visitedRepo, analytics)
 
-            viewModel.onPageView()
+        every { visitedRepo.visitedItems } returns flowOf(visitedItems)
 
-            runTest {
-                val result = viewModel.uiState.first()
+        viewModel.onPageView()
 
-                assertEquals(visitedItems, result)
-            }
+        runTest {
+            assertEquals(expected, viewModel.uiState.first())
         }
     }
 }
