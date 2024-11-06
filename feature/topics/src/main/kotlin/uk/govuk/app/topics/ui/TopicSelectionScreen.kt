01c862e7564009fce8a9944fa57a60bd111b48e7
David Carr <david.carr@digital.cabinet-office.gov.uk>
2024-11-06 13:50:42 +0000

    Make items in grid height of the tallest item in the row

--- feature/topics/src/main/kotlin/uk/govuk/app/topics/ui/TopicSelectionScreen.kt
+++ feature/topics/src/main/kotlin/uk/govuk/app/topics/ui/TopicSelectionScreen.kt
@@ -1,10 +1,13 @@
 package uk.govuk.app.topics.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -13,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import uk.govuk.app.design.ui.component.ChildPageHeader
 import uk.govuk.app.design.ui.component.MediumVerticalSpacer
+import uk.govuk.app.design.ui.component.SmallHorizontalSpacer
 import uk.govuk.app.design.ui.theme.GovUkTheme
 import uk.govuk.app.topics.ui.component.TopicSelectionCard
 import uk.govuk.app.topics.ui.model.TopicItemUi
@@ -64,24 +68,89 @@ private fun TopicSelectionScreen(
         )
 
         if (!topics.isNullOrEmpty()) {
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
-                Modifier.padding(horizontal = GovUkTheme.spacing.medium),
-            ) {
-                item {
-                    MediumVerticalSpacer()
-                }
+            TopicsGrid(
+                topics = topics,
+                onClick = { _, _ -> }
+            )
+        }
+    }
+}
 
-                items(topics) { topic ->
-                    TopicSelectionCard(
-                        icon = topic.icon,
-                        title = topic.title,
-                        description = topic.description,
-//                        onClick = { onClick(topic.title) }
-                        isSelected = false
-                    )
-                }
+@Composable
+private fun TopicsGrid(
+    topics: List<TopicItemUi>,
+    onClick: (String, String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val columnCount = 2
+
+    LazyColumn(
+        modifier.padding(horizontal = GovUkTheme.spacing.medium),
+    ) {
+        item {
+            MediumVerticalSpacer()
+        }
+
+        items(
+            getRowCount(
+                topicsCount = topics.size,
+                columnCount = columnCount
+            )
+        ) { rowIndex ->
+            TopicsRow(
+                topics = topics,
+                columnCount = columnCount,
+                rowIndex = rowIndex,
+                onClick = onClick
+            )
+            MediumVerticalSpacer()
+        }
+    }
+}
+
+@Composable
+private fun TopicsRow(
+    topics: List<TopicItemUi>,
+    columnCount: Int,
+    rowIndex: Int,
+    onClick: (String, String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier.height(intrinsicSize = IntrinsicSize.Max)
+    ) {
+        for (columnIndex in 0 until columnCount) {
+            if (columnIndex > 0) {
+                SmallHorizontalSpacer()
+            }
+
+            val topicIndex = (rowIndex * columnCount) + columnIndex
+            if (topicIndex < topics.size) {
+                val topic = topics[topicIndex]
+                TopicSelectionCard(
+                    icon = topic.icon,
+                    title =  topic.title,
+                    description = topic.description,
+                    isSelected = false,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f)
+                )
+            } else {
+                Box(Modifier.weight(1f)) {  }
+            }
+
+            if (columnIndex < columnCount - 1) {
+                SmallHorizontalSpacer()
             }
         }
     }
+}
+
+private fun getRowCount(topicsCount: Int, columnCount: Int): Int {
+    var rowCount = topicsCount / columnCount
+    if (topicsCount.mod(columnCount) > 0) {
+        rowCount += 1
+    }
+    return rowCount
 }
