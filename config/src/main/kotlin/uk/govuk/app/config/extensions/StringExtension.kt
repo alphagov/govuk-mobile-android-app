0a802dcc59b37b4918acb61acaaca50ade4d1d04
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2024-11-12 14:56:16 +0000

    Rename val's to names less specific to app version

--- config/src/main/kotlin/uk/govuk/app/config/extensions/StringExtension.kt
+++ config/src/main/kotlin/uk/govuk/app/config/extensions/StringExtension.kt
@@ -3,19 +3,19 @@ package uk.govuk.app.config.extensions
 fun String.isVersionLessThan(targetVersion: String): Boolean {
     val versionDelimiter = "."
     val targetVersionList = targetVersion.split(versionDelimiter).map { it.toIntOrNull() }
-    val appVersionList = this.split(versionDelimiter).map { it.toIntOrNull() }
-    val theSmallerListSize = minOf(targetVersionList.size, appVersionList.size)
+    val versionList = this.split(versionDelimiter).map { it.toIntOrNull() }
+    val smallestListSize = minOf(targetVersionList.size, versionList.size)
 
-    for (i in 0 until theSmallerListSize) {
-        val appVersionNumberSegment = appVersionList[i] ?: 0
+    for (i in 0 until smallestListSize) {
+        val versionNumberSegment = versionList[i] ?: 0
         val targetVersionNumberSegment = targetVersionList[i] ?: 0
-        return if (targetVersionNumberSegment > appVersionNumberSegment) {
+        return if (targetVersionNumberSegment > versionNumberSegment) {
             true
-        } else if (targetVersionNumberSegment == appVersionNumberSegment) {
+        } else if (targetVersionNumberSegment == versionNumberSegment) {
             continue
         } else {
             false
         }
     }
-    return targetVersionList.size > appVersionList.size
+    return targetVersionList.size > versionList.size
 }
