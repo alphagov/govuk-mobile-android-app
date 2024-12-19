0ddb02a26811c4d396a98ab39843032c90feb43e
David Carr <david.carr@digital.cabinet-office.gov.uk>
2024-12-19 14:51:40 +0000

    Talkback

--- feature/settings/src/main/kotlin/uk/govuk/app/settings/ui/FeedbackPromptWidget.kt
+++ feature/settings/src/main/kotlin/uk/govuk/app/settings/ui/FeedbackPromptWidget.kt
@@ -1,8 +1,11 @@
 package uk.govuk.app.settings.ui
 
+import androidx.compose.foundation.focusable
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.tooling.preview.Preview
 import uk.govuk.app.design.ui.component.HomeNavigationCard
 import uk.govuk.app.design.ui.theme.GovUkTheme
@@ -14,10 +17,16 @@ fun FeedbackPromptWidget(
     modifier: Modifier = Modifier
 ) {
     val title = stringResource(R.string.feedback_prompt_widget_title)
+    val altText = "$title. ${stringResource(R.string.link_opens_in)}"
+
     HomeNavigationCard(
         title = title,
         onClick = { onClick(title) },
-        modifier = modifier,
+        modifier = modifier
+            .focusable()
+            .clearAndSetSemantics {
+                contentDescription = altText
+            },
         isSelected = true
     )
 }
