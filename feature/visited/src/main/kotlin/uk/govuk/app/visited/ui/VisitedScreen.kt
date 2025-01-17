c69f99dc84dd718e04d216aa96ecfea222ad162c
Tim Denyer <30877434+TimGJD@users.noreply.github.com>
2025-01-17 09:06:35 +0000

    GOVUKAPP-1025: Edit button context (#167)
    Add alt-text to edit and done buttons

--- feature/visited/src/main/kotlin/uk/govuk/app/visited/ui/VisitedScreen.kt
+++ feature/visited/src/main/kotlin/uk/govuk/app/visited/ui/VisitedScreen.kt
@@ -81,7 +81,9 @@ private fun VisitedScreen(
     }
 
     val title = stringResource(R.string.visited_items_title)
+    val backAltText = stringResource(R.string.visited_items_back_button_alt_text)
     val editText = stringResource(R.string.visited_items_edit_button)
+    val editAltText = stringResource(R.string.visited_items_edit_button_alt_text)
     val visitedItems = uiState?.visited
     val lifecycleOwner = LocalLifecycleOwner.current
     val lifecycleState by lifecycleOwner.lifecycle.currentStateFlow.collectAsState()
@@ -94,8 +96,10 @@ private fun VisitedScreen(
         ChildPageHeader(
             text = title,
             onBack = onBack,
+            backAltText = backAltText,
             onAction = onAction,
             actionText = editText,
+            actionAltText = editAltText,
             modifier = Modifier.focusRequester(focusRequester)
         )
 
