246975355664155c4dda15ffbca7a245a89865d2
David Carr <david.carr@digital.cabinet-office.gov.uk>
2025-01-20 11:25:14 +0000

    Remove redundant data store functionality and tests

--- feature/topics/src/main/kotlin/uk/govuk/app/topics/data/local/TopicsDataStore.kt
+++ feature/topics/src/main/kotlin/uk/govuk/app/topics/data/local/TopicsDataStore.kt
@@ -4,7 +4,6 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.firstOrNull
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -15,29 +14,7 @@ internal class TopicsDataStore @Inject constructor(
 ) {
 
     companion object {
-        internal const val REALM_TOPICS_KEY = "realm_topics_key"
-        internal const val REALM_TOPICS_IV = "realm_topics_iv"
         internal const val TOPICS_CUSTOMISED = "topics_customised"
-    }
-
-    internal suspend fun getRealmTopicsKey(): String? {
-        return dataStore.data.firstOrNull()?.get(stringPreferencesKey(REALM_TOPICS_KEY))
-    }
-
-    internal suspend fun saveRealmTopicsKey(key: String) {
-        dataStore.edit { preferences ->
-            preferences[stringPreferencesKey(REALM_TOPICS_KEY)] = key
-        }
-    }
-
-    internal suspend fun getRealmTopicsIv(): String? {
-        return dataStore.data.firstOrNull()?.get(stringPreferencesKey(REALM_TOPICS_IV))
-    }
-
-    internal suspend fun saveRealmTopicsIv(iv: String) {
-        dataStore.edit { preferences ->
-            preferences[stringPreferencesKey(REALM_TOPICS_IV)] = iv
-        }
     }
 
     internal suspend fun isTopicsCustomised(): Boolean {
--- feature/topics/src/test/kotlin/uk/govuk/app/topics/data/local/TopicsDataStoreTest.kt
+++ feature/topics/src/test/kotlin/uk/govuk/app/topics/data/local/TopicsDataStoreTest.kt
@@ -3,14 +3,11 @@ package uk.govuk.app.topics.data.local
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.stringPreferencesKey
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
-import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -18,54 +15,6 @@ class TopicsDataStoreTest {
 
     private val dataStore = mockk<DataStore<Preferences>>()
     private val preferences = mockk<Preferences>()
-
-    @Test
-    fun `Given realm key is null, then return null`() {
-        val datastore = TopicsDataStore(dataStore)
-
-        every { dataStore.data } returns flowOf(preferences)
-        every { preferences[stringPreferencesKey(TopicsDataStore.REALM_TOPICS_KEY)] } returns null
-
-        runTest {
-            assertNull(datastore.getRealmTopicsKey())
-        }
-    }
-
-    @Test
-    fun `Given realm key is not null, then return realm key`() {
-        val datastore = TopicsDataStore(dataStore)
-
-        every { dataStore.data } returns flowOf(preferences)
-        every { preferences[stringPreferencesKey(TopicsDataStore.REALM_TOPICS_KEY)] } returns "realmKey"
-
-        runTest {
-            assertEquals("realmKey", datastore.getRealmTopicsKey())
-        }
-    }
-
-    @Test
-    fun `Given realm iv is null, then return null`() {
-        val datastore = TopicsDataStore(dataStore)
-
-        every { dataStore.data } returns flowOf(preferences)
-        every { preferences[stringPreferencesKey(TopicsDataStore.REALM_TOPICS_IV)] } returns null
-
-        runTest {
-            assertNull(datastore.getRealmTopicsIv())
-        }
-    }
-
-    @Test
-    fun `Given realm iv is not null, then return realm iv`() {
-        val datastore = TopicsDataStore(dataStore)
-
-        every { dataStore.data } returns flowOf(preferences)
-        every { preferences[stringPreferencesKey(TopicsDataStore.REALM_TOPICS_IV)] } returns "realmIv"
-
-        runTest {
-            assertEquals("realmIv", datastore.getRealmTopicsIv())
-        }
-    }
 
     @Test
     fun `Given topics customised is null, then return false`() {
