bfcb3a9f17015664ea16f04911659ceae200d97d
David Carr <david.carr@digital.cabinet-office.gov.uk>
2025-03-10 14:07:16 +0000

    Persist search term across orientation change

--- feature/search/src/main/kotlin/uk/govuk/app/search/ui/SearchScreen.kt
+++ feature/search/src/main/kotlin/uk/govuk/app/search/ui/SearchScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -109,7 +110,7 @@ private fun SearchScreen(
     val focusRequester = remember { FocusRequester() }
     val keyboard = LocalSoftwareKeyboardController.current
 
-    var searchTerm by remember { mutableStateOf("") }
+    var searchTerm by rememberSaveable { mutableStateOf("") }
 
     val interactionSource = remember { MutableInteractionSource() }
 
