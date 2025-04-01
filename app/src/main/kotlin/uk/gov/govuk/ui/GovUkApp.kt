d4650fe15cb2f917bbee17016cde3d8710015aa9
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2025-04-01 10:18:37 +0100

    Add bottom padding for edge to edge

--- app/src/main/kotlin/uk/gov/govuk/ui/GovUkApp.kt
+++ app/src/main/kotlin/uk/gov/govuk/ui/GovUkApp.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
@@ -163,12 +165,14 @@ private fun BottomNavScaffold(
     onDeepLinkReceived: (Boolean, String) -> Unit
 ) {
     val navController = rememberNavController()
+    val navBarPadding = WindowInsets.navigationBars.asPaddingValues()
 
     Scaffold(
         contentWindowInsets = WindowInsets(0.dp),
         bottomBar = {
             BottomNav(navController, onTabClick)
-        }
+        },
+        modifier = Modifier.padding(bottom = navBarPadding.calculateBottomPadding())
     ) { paddingValues ->
         Surface(
             modifier = Modifier.fillMaxSize(),
