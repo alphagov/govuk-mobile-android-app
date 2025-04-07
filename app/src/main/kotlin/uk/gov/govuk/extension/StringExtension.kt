461573f51d5870e863699f6f3e7587c97f8a96eb
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2025-04-07 15:57:59 +0100

    Remove unused import

--- app/src/main/kotlin/uk/gov/govuk/extension/StringExtension.kt
+++ app/src/main/kotlin/uk/gov/govuk/extension/StringExtension.kt
@@ -2,17 +2,16 @@ package uk.gov.govuk.extension
 
 import androidx.navigation.NavDeepLink
 import androidx.navigation.navDeepLink
-import uk.gov.govuk.navigation.DeepLink
 
 /**
- * Formats a deep link path into uri patterns for each allowed url
+ * Formats a deep link path into a uri pattern for each url.
  *
  * Returns a collection of NavDeepLink objects for each formatted uri pattern.
  */
-internal fun String.asDeepLinks(allowedUrls: List<String>): List<NavDeepLink> {
+internal fun String.asDeepLinks(urls: List<String>): List<NavDeepLink> {
     val path = this
     val navDeepLinks = mutableListOf<NavDeepLink>()
-    allowedUrls.forEach {
+    urls.forEach {
         navDeepLinks.add(navDeepLink {
             uriPattern = "$it$path"
         })
