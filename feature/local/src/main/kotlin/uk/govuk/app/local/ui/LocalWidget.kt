b291c3f68b1c2c84d97c05261e199e4af283035b
David Carr <david.carr@digital.cabinet-office.gov.uk>
2025-04-14 09:49:50 +0100

    Start on local authrity card for widget

--- feature/local/src/main/kotlin/uk/govuk/app/local/ui/LocalWidget.kt
+++ feature/local/src/main/kotlin/uk/govuk/app/local/ui/LocalWidget.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,7 +15,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -22,7 +23,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import uk.gov.govuk.design.ui.component.BodyBoldLabel
@@ -59,8 +62,38 @@ private fun LocalAuthorityCard(
     localAuthority: LocalAuthorityUi,
     modifier: Modifier = Modifier
 ) {
-    Column {
-        Text("$localAuthority")
+    Column(modifier) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Title3BoldLabel(
+                text = "Your local services",
+                modifier = Modifier.semantics { heading() }
+            )
+
+            Spacer(Modifier.weight(1f))
+
+            val editButtonText = "Edit"
+            val editButtonAltText = "Edit alt text"
+
+            TextButton(
+                onClick = { }
+            ) {
+                BodyRegularLabel(
+                    text = editButtonText,
+                    color = GovUkTheme.colourScheme.textAndIcons.link,
+                    modifier = Modifier.semantics {
+                        contentDescription = editButtonAltText
+                    }
+                )
+            }
+        }
+
+        if (localAuthority.parent != null) {
+            BodyRegularLabel("Services in your area are provided by 2 local councils")
+        }
     }
 }
 
@@ -131,13 +164,5 @@ private fun NoLocalAuthorityCard(
                 )
             }
         }
-    }
-}
-
-@Preview
-@Composable
-private fun LocalWidgetPreview() {
-    GovUkTheme {
-        LocalWidget(onClick = { })
     }
 }
