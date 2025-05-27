dd736a2f63ac3b7674d3d697f1be17e732b5f548
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2025-05-27 14:04:52 +0100

    Replace existing 3-page onboarding screens with single page

--- feature/onboarding/src/main/kotlin/uk/gov/govuk/onboarding/OnboardingViewModel.kt
+++ feature/onboarding/src/main/kotlin/uk/gov/govuk/onboarding/OnboardingViewModel.kt
@@ -1,74 +1,30 @@
 package uk.gov.govuk.onboarding
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import uk.gov.govuk.analytics.AnalyticsClient
 import javax.inject.Inject
 
-internal data class OnboardingUiState(
-    val pages: List<OnboardingPage>
-)
-
 @HiltViewModel
 internal class OnboardingViewModel @Inject constructor(
-    @ApplicationContext private val context: Context,
     private val analyticsClient: AnalyticsClient
 ): ViewModel() {
 
     companion object {
-        private const val ONBOARDING_SCREEN_1_NAME = "Onboarding_A"
-        private const val ONBOARDING_SCREEN_2_NAME = "Onboarding_B"
-        private const val ONBOARDING_SCREEN_3_NAME = "Onboarding_C"
-
-        private const val SCREEN_CLASS = "OnboardingScreen"
+        private const val ONBOARDING_SCREEN_CLASS = "OnboardingScreen"
+        private const val ONBOARDING_SCREEN_NAME = "Onboarding Page"
+        private const val ONBOARDING_TITLE = "Onboarding Page"
     }
 
-    // Todo - this will probably come from JSON file or remote config etc
-    private val _uiState: MutableStateFlow<OnboardingUiState> = MutableStateFlow(
-        OnboardingUiState(
-            listOf(
-                OnboardingPage(
-                    screenName = ONBOARDING_SCREEN_1_NAME,
-                    title = R.string.getThingsDoneScreenTitle,
-                    body = R.string.getThingsDoneScreenBody,
-                    image = R.drawable.image_1,
-                ),
-                OnboardingPage(
-                    screenName = ONBOARDING_SCREEN_2_NAME,
-                    title = R.string.backToPreviousScreenTitle,
-                    body = R.string.backToPreviousScreenBody,
-                    image = R.drawable.image_2
-                ),
-                OnboardingPage(
-                    screenName = ONBOARDING_SCREEN_3_NAME,
-                    title = R.string.tailoredToYouScreenTitle,
-                    body = R.string.tailoredToYouScreenBody,
-                    image = R.drawable.image_3
-                ),
-            )
-        )
-    )
-    val uiState = _uiState.asStateFlow()
-
-    fun onPageView(pageIndex: Int) {
-        val page = uiState.value.pages[pageIndex]
+    fun onPageView() {
         analyticsClient.screenView(
-            screenClass = SCREEN_CLASS,
-            screenName = page.screenName,
-            title = context.getString(page.title)
+            screenClass = ONBOARDING_SCREEN_CLASS,
+            screenName = ONBOARDING_SCREEN_NAME,
+            title = ONBOARDING_TITLE
         )
     }
 
     fun onButtonClick(text: String) {
         analyticsClient.buttonClick(text)
     }
-
-    fun onPagerIndicatorClick() {
-        analyticsClient.pageIndicatorClick()
-    }
-
 }
