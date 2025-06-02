6f2d2a2fc9904ffdeb596118637c0334ef1177da
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2025-06-02 08:12:34 +0100

    Replace extra function with function param

--- app/src/main/kotlin/uk/gov/govuk/ui/BrowserLauncher.kt
+++ app/src/main/kotlin/uk/gov/govuk/ui/BrowserLauncher.kt
@@ -12,6 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.core.net.toUri
 
+internal enum class Presentation {
+    FULL_SCREEN,
+    PARTIAL_SCREEN
+}
+
 @Composable
 internal fun rememberBrowserLauncher(shouldShowInAppBrowser: Boolean): BrowserActivityLauncher {
     val launcher =
@@ -28,15 +33,16 @@ internal fun rememberBrowserLauncher(shouldShowInAppBrowser: Boolean): BrowserAc
 internal sealed class BrowserActivityLauncher(
     val launcher: ManagedActivityResultLauncher<Intent, ActivityResult>
 ) {
-    abstract fun launch(context: Context, url: String)
-    open fun launchStacked(context: Context, url: String) {
-        launch(context, url)
-    }
+    abstract fun launch(
+        context: Context,
+        url: String,
+        presentation: Presentation = Presentation.FULL_SCREEN
+    )
 
     internal class External(
         launcher: ManagedActivityResultLauncher<Intent, ActivityResult>
     ) : BrowserActivityLauncher(launcher) {
-        override fun launch(context: Context, url: String) {
+        override fun launch(context: Context, url: String, presentation: Presentation) {
             Intent(Intent.ACTION_VIEW).run {
                 data = url.toUri()
                 launcher.launch(this)
@@ -47,23 +53,18 @@ internal sealed class BrowserActivityLauncher(
     internal class InApp(
         launcher: ManagedActivityResultLauncher<Intent, ActivityResult>
     ) : BrowserActivityLauncher(launcher) {
-        override fun launch(context: Context, url: String) {
-            CustomTabsIntent.Builder().build().run {
-                intent.data = url.toUri()
-                launcher.launch(intent)
+        override fun launch(context: Context, url: String, presentation: Presentation) {
+            val customTabsIntent = when (presentation) {
+                Presentation.FULL_SCREEN -> CustomTabsIntent.Builder().build()
+                Presentation.PARTIAL_SCREEN -> context.getPartialCustomTabsIntent()
             }
-        }
-
-        override fun launchStacked(context: Context, url: String) {
-            context.getStackedBrowserIntent().run {
-                intent.data = url.toUri()
-                launcher.launch(intent)
-            }
+            customTabsIntent.intent.data = url.toUri()
+            launcher.launch(customTabsIntent.intent)
         }
     }
 }
 
-private fun Context.getStackedBrowserIntent(): CustomTabsIntent {
+private fun Context.getPartialCustomTabsIntent(): CustomTabsIntent {
     val displayMetrics = this.resources.displayMetrics
     val screenHeight = displayMetrics.heightPixels
     return CustomTabsIntent.Builder()
