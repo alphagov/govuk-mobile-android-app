109ff026432417fe7856a451020acdcf9add5345
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2025-06-25 21:12:58 +0100

    Try to resolve Sonar code coverage issues

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
 
     testImplementation(libs.junit)
     testImplementation(libs.mockk)
+    testImplementation(kotlin("test"))
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
--- auth/src/test/java/uk/gov/govuk/auth/AuthenticationViewModelTest.kt
+++ auth/src/test/java/uk/gov/govuk/auth/AuthenticationViewModelTest.kt
@@ -3,24 +3,18 @@ package uk.gov.govuk.auth
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.Test
-import org.junit.Before
 import uk.gov.govuk.data.auth.AuthRepo
 
 class AuthenticationViewModelTest {
     private val authRepo = mockk<AuthRepo>(relaxed = true)
 
-    private lateinit var viewModel: AuthenticationViewModel
-
-    @Before
-    fun setup() {
-        viewModel = AuthenticationViewModel(authRepo)
-    }
-
     @Test
     fun `Given user session is active, then authentication state should be logged in`() {
         every { authRepo.isUserSessionActive() } returns true
 
+        val viewModel = AuthenticationViewModel(authRepo)
         val state = viewModel.authenticationState
+
         assert(state == AuthenticationState.LoggedIn)
     }
 
@@ -28,7 +22,9 @@ class AuthenticationViewModelTest {
     fun `Given user session is not active, then authentication state should be not logged in`() {
         every { authRepo.isUserSessionActive() } returns false
 
+        val viewModel = AuthenticationViewModel(authRepo)
         val state = viewModel.authenticationState
+
         assert(state == AuthenticationState.NotLoggedIn)
     }
 }
