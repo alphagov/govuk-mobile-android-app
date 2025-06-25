dde90e0404f237228f52dc6e563f2a49b15fa011
David Carr <david.carr@digital.cabinet-office.gov.uk>
2025-06-25 12:04:12 +0100

    GOVUKAPP-1869: Search user agent (#331)
    Send custom user agent for search and autocomplete

--- feature/search/src/main/kotlin/uk/gov/govuk/search/di/SearchModule.kt
+++ feature/search/src/main/kotlin/uk/gov/govuk/search/di/SearchModule.kt
@@ -1,9 +1,14 @@
 package uk.gov.govuk.search.di
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import uk.gov.govuk.search.DefaultSearchFeature
@@ -26,22 +31,44 @@ internal class SearchModule {
 
     @Provides
     @Singleton
-    fun providesSearchApi(): SearchApi {
+    fun providesSearchApi(@ApplicationContext context: Context): SearchApi {
+        val httpClient = OkHttpClient.Builder()
+            .addInterceptor(UserAgentInterceptor(context))
+            .build()
+
         return Retrofit.Builder()
             .baseUrl(SearchConfig.API_BASE_URL)
+            .client(httpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(SearchApi::class.java)
     }
 
-
     @Provides
     @Singleton
-    fun providesAutocompleteApi(): AutocompleteApi {
+    fun providesAutocompleteApi(@ApplicationContext context: Context): AutocompleteApi {
+        val httpClient = OkHttpClient.Builder()
+            .addInterceptor(UserAgentInterceptor(context))
+            .build()
+
         return Retrofit.Builder()
             .baseUrl(SearchConfig.BASE_URL)
+            .client(httpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(AutocompleteApi::class.java)
+    }
+
+    private class UserAgentInterceptor(@ApplicationContext context: Context): Interceptor {
+        private val appVersion = context.packageManager.getPackageInfo(context.packageName, 0).versionName
+        private val userAgent = "govuk_android/$appVersion"
+
+        override fun intercept(chain: Interceptor.Chain): Response {
+            return chain.proceed(
+                chain.request().newBuilder()
+                    .header("User-Agent", userAgent)
+                    .build()
+            )
+        }
     }
 }
