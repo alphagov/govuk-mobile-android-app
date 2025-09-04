7384ebe43b5b9711ebec87e36312021f5fb5094d
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2025-09-04 17:35:53 +0100

    Remove unused import

--- design/src/main/kotlin/uk/gov/govuk/design/ui/component/Card.kt
+++ design/src/main/kotlin/uk/gov/govuk/design/ui/component/Card.kt
@@ -2,7 +2,6 @@ package uk.gov.govuk.design.ui.component
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
