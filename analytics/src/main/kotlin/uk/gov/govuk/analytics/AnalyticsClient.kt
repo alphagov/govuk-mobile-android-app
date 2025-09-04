c7eb070419d4482a5f2e59cdeb6a2858842126b9
David Carr <david.carr@digital.cabinet-office.gov.uk>
2025-09-04 11:10:22 +0100

    Add action to chat stat

--- analytics/src/main/kotlin/uk/gov/govuk/analytics/AnalyticsClient.kt
+++ analytics/src/main/kotlin/uk/gov/govuk/analytics/AnalyticsClient.kt
@@ -102,6 +102,7 @@ class AnalyticsClient @Inject constructor(
         logEvent(
             "Chat",
             mapOf(
+                "action" to "Ask Question",
                 "type" to "typed"
             )
         )
