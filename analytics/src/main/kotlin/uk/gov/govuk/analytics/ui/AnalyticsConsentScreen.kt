6f5b1ed4f41878567497f2363a63836224bc5f61
Tim Denyer <30877434+TimGJD@users.noreply.github.com>
2025-09-08 16:14:24 +0100

    GOVUKAPP-2402 Analytics content change (#424)
    * Update analytics content to match current designs
    
    * Remove unused alert banner usages
    
    * Align bullet point spacing with designs
    
    * Align padding with the designs

--- analytics/src/main/kotlin/uk/gov/govuk/analytics/ui/AnalyticsConsentScreen.kt
+++ analytics/src/main/kotlin/uk/gov/govuk/analytics/ui/AnalyticsConsentScreen.kt
@@ -1,12 +1,15 @@
 package uk.gov.govuk.analytics.ui
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
@@ -14,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
@@ -60,6 +64,9 @@ private fun AnalyticsConsentScreen(
     launchBrowser: (url: String) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val isPortrait =
+        LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT
+
     Column(
         modifier
             .fillMaxWidth()
@@ -69,7 +76,10 @@ private fun AnalyticsConsentScreen(
                 .weight(1f)
                 .verticalScroll(rememberScrollState())
                 .padding(horizontal = GovUkTheme.spacing.medium)
-                .padding(top = GovUkTheme.spacing.medium)
+                .padding(
+                    top = if (isPortrait) GovUkTheme.spacing.extraLarge * 2
+                    else GovUkTheme.spacing.extraLarge
+                )
         ) {
             LargeTitleBoldLabel(
                 text = stringResource(R.string.analytics_consent_title),
@@ -124,6 +134,7 @@ private fun BulletItem(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
     ) {
+        Spacer(modifier = modifier.width(10.dp))
         Box(
             modifier = Modifier
                 .size(6.dp)
