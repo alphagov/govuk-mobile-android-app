30c69f357893d1e23824340802134c3d6db7a2b6
David Carr <david.carr@digital.cabinet-office.gov.uk>
2025-09-15 10:52:11 +0100

    Generating answer animations

--- feature/chat/src/main/kotlin/uk/gov/govuk/chat/ui/chat/ChatEntry.kt
+++ feature/chat/src/main/kotlin/uk/gov/govuk/chat/ui/chat/ChatEntry.kt
@@ -1,19 +1,27 @@
 package uk.gov.govuk.chat.ui.chat
 
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
+import android.widget.ImageView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.viewinterop.AndroidView
+import kotlinx.coroutines.delay
 import uk.gov.govuk.chat.R
 import uk.gov.govuk.chat.ui.model.ChatEntry
 import uk.gov.govuk.design.ui.component.MediumVerticalSpacer
+import uk.gov.govuk.design.ui.component.SmallHorizontalSpacer
 import uk.gov.govuk.design.ui.theme.GovUkTheme
 
 @Composable
@@ -54,16 +62,30 @@ private fun Loading(
         modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            painter = painterResource(R.drawable.baseline_circle_24),
-            contentDescription = null,
-            tint = GovUkTheme.colourScheme.textAndIcons.chatLoadingIcon,
-            modifier = Modifier
-                .padding(end = GovUkTheme.spacing.small),
-        )
+        AndroidView(factory = {
+            ImageView(it).apply {
+                val source = ImageDecoder.createSource(context.resources, R.drawable.ic_generating_answer)
+                val drawable = ImageDecoder.decodeDrawable(source)
+                setImageDrawable(drawable)
+                if (drawable is AnimatedImageDrawable) {
+                    drawable.start()
+                }
+            }
+        })
+
+        SmallHorizontalSpacer()
+
+        var dots by remember { mutableIntStateOf(0) }
+
+        LaunchedEffect(Unit) {
+            while (true) {
+                delay(1200L)
+                dots = (dots + 1) % 4 // 0,1,2,3
+            }
+        }
 
         Text(
-            text = stringResource(R.string.loading_text),
+            text = stringResource(R.string.loading_text) + ".".repeat(dots),
             color = GovUkTheme.colourScheme.textAndIcons.chatLoadingTextDark,
             style = GovUkTheme.typography.bodyRegular
         )
