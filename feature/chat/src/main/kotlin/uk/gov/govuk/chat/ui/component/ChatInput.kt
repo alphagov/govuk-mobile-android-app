de9f22a6f5cc98e28468530e35491d053a5a0815
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2025-09-25 12:50:59 +0100

    Fix Sonar issue

--- feature/chat/src/main/kotlin/uk/gov/govuk/chat/ui/component/ChatInput.kt
+++ feature/chat/src/main/kotlin/uk/gov/govuk/chat/ui/component/ChatInput.kt
@@ -100,11 +100,7 @@ internal fun ChatInput(
             Box(
                 modifier = Modifier
                     .fillMaxWidth(
-                        if (isFocused && uiState.question.isNotEmpty()) {
-                            1f
-                        } else {
-                            0.88f
-                        }
+                        if (focusedWithInput(isFocused, uiState)) 1f else 0.88f
                     )
                     .animateContentSize(
                         animationSpec = tween(durationMillis = 100)
@@ -144,7 +140,7 @@ internal fun ChatInput(
                     colors = inputTextFieldDefaults(),
                     trailingIcon = {
                         AnimateIcon(
-                            isFocused && uiState.question.isNotEmpty(),
+                            focusedWithInput(isFocused, uiState),
                             {
                                 SubmitIconButton(
                                     onClick = {
@@ -160,7 +156,7 @@ internal fun ChatInput(
             }
 
             AnimateIcon(
-                (uiState.question.isEmpty() || !isFocused && uiState.question.isNotEmpty()),
+                (uiState.question.isEmpty() || !focusedWithInput(isFocused, uiState)),
                 {
                     ActionMenu(
                         hasConversation = hasConversation,
@@ -179,6 +175,13 @@ internal fun ChatInput(
             )
         }
     }
+}
+
+private fun focusedWithInput(
+    isFocused: Boolean,
+    uiState: ChatUiState.Default
+) : Boolean {
+    return isFocused && uiState.question.isNotEmpty()
 }
 
 @Composable
