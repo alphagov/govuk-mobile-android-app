bec0fcc94c75e6848d0e2034f6ed102f6b07cc0f
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2025-10-03 10:13:03 +0100

    Add window insets padding to screens for system bars

--- design/src/main/kotlin/uk/gov/govuk/design/ui/component/error/AppUnavailableScreen.kt
+++ design/src/main/kotlin/uk/gov/govuk/design/ui/component/error/AppUnavailableScreen.kt
@@ -2,8 +2,11 @@ package uk.gov.govuk.design.ui.component.error
 
 import android.content.Intent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -24,7 +27,9 @@ fun AppUnavailableScreen(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier.fillMaxWidth()
+        modifier
+            .fillMaxWidth()
+            .windowInsetsPadding(WindowInsets.systemBars)
     ) {
         Column(
             Modifier
