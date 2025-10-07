da1478cab1b812c28edef11b189eac71e812edf3
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2025-10-07 15:26:07 +0100

    Add content description to optional button

--- design/src/main/kotlin/uk/gov/govuk/design/ui/component/List.kt
+++ design/src/main/kotlin/uk/gov/govuk/design/ui/component/List.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
@@ -101,8 +102,9 @@ fun SectionHeading(
                         .background(GovUkTheme.colourScheme.surfaces.cardDefault)
                         .padding(
                             horizontal = GovUkTheme.spacing.medium,
-                            vertical = GovUkTheme.spacing.small
-                        ),
+                            vertical = 9.dp
+                        )
+                        .semantics { contentDescription = button.altText },
                     color = GovUkTheme.colourScheme.textAndIcons.link
                 )
             }
