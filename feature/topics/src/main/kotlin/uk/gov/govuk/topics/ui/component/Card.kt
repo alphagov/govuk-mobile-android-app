ff40889a9859ed936821d339d9d60dd1c011b0ce
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2025-10-17 11:11:24 +0100

    Create new topic selection card component

--- feature/topics/src/main/kotlin/uk/gov/govuk/topics/ui/component/Card.kt
+++ feature/topics/src/main/kotlin/uk/gov/govuk/topics/ui/component/Card.kt
@@ -1,40 +1,25 @@
 package uk.gov.govuk.topics.ui.component
 
+import android.content.res.Configuration
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Check
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextLayoutResult
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import uk.gov.govuk.design.ui.component.BodyBoldLabel
-import uk.gov.govuk.design.ui.component.BodyRegularLabel
 import uk.gov.govuk.design.ui.component.GovUkCard
 import uk.gov.govuk.design.ui.component.MediumHorizontalSpacer
-import uk.gov.govuk.design.ui.component.MediumVerticalSpacer
-import uk.gov.govuk.design.ui.component.SmallHorizontalSpacer
-import uk.gov.govuk.design.ui.component.SmallVerticalSpacer
-import uk.gov.govuk.design.ui.component.SubheadlineRegularLabel
-import uk.gov.govuk.design.ui.component.Title3BoldLabel
 import uk.gov.govuk.design.ui.theme.GovUkTheme
 import uk.gov.govuk.topics.R
 
@@ -42,165 +27,59 @@ import uk.gov.govuk.topics.R
 fun TopicSelectionCard(
     @DrawableRes icon: Int,
     title: String,
-    description: String,
     isSelected: Boolean,
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-
     GovUkCard(
-        modifier = modifier,
+        modifier,
         isSelected = isSelected,
-        onClick = onClick
-    ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Icon(
-                painterResource(icon),
-                contentDescription = null,
-                tint = GovUkTheme.colourScheme.textAndIcons.icon
-            )
-            SmallVerticalSpacer()
-            Title3BoldLabel(
-                text = title,
-                textAlign = TextAlign.Center
-            )
-            SubheadlineRegularLabel(
-                text = description,
-                textAlign = TextAlign.Center
-            )
-            MediumVerticalSpacer()
-            Spacer(Modifier.weight(1f))
-            if (isSelected) {
-                SelectedButton()
-            } else {
-                SelectButton()
-            }
-        }
-    }
-}
-
-@Composable
-private fun SelectButton(
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            imageVector = Icons.Filled.Add,
-            contentDescription = null,
-            tint = GovUkTheme.colourScheme.textAndIcons.link
-        )
-        BodyRegularLabel(
-            text = stringResource(R.string.select_button),
-            modifier = Modifier.padding(top = 2.dp),
-            color = GovUkTheme.colourScheme.textAndIcons.link
-        )
-    }
-}
-
-@Composable
-private fun SelectedButton(
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            imageVector = Icons.Filled.Check,
-            contentDescription = null,
-            tint = GovUkTheme.colourScheme.textAndIcons.buttonSuccess
-        )
-        Spacer(Modifier.width(2.dp))
-        BodyRegularLabel(
-            text = stringResource(R.string.selected_button),
-            modifier = Modifier.padding(top = 2.dp),
-            color = GovUkTheme.colourScheme.textAndIcons.buttonSuccess
-        )
-    }
-}
-
-@Composable
-fun TopicVerticalCard(
-    @DrawableRes icon: Int,
-    title: String,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    GovUkCard(
-        modifier,
-        onClick = onClick
-    ) {
-        Icon(
-            painterResource(icon),
-            contentDescription = null,
-            tint = GovUkTheme.colourScheme.textAndIcons.icon
-        )
-        SmallVerticalSpacer()
-        Spacer(Modifier.weight(1f))
-        Row {
-            var baseline by remember { mutableFloatStateOf(0f) }
-            fun updateBaseline(textLayoutResult: TextLayoutResult) {
-                baseline = textLayoutResult.size.height - textLayoutResult.lastBaseline
-            }
-            val baselinePadding = with(LocalDensity.current) { baseline.toDp() }
-
-            BodyBoldLabel(
-                text = title,
-                modifier = Modifier.weight(1f),
-                onTextLayout = ::updateBaseline
-            )
-            SmallHorizontalSpacer()
-            Icon(
-                painterResource(uk.gov.govuk.design.R.drawable.ic_chevron),
-                contentDescription = null,
-                modifier = Modifier
-                    .align(Alignment.Bottom)
-                    .padding(bottom = baselinePadding),
-                tint = GovUkTheme.colourScheme.textAndIcons.icon
-            )
-        }
-    }
-}
-
-@Composable
-fun TopicHorizontalCard(
-    @DrawableRes icon: Int,
-    title: String,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    GovUkCard(
-        modifier,
         onClick = { onClick() }
     ) {
         Row(
+            modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
-        ){
-            Icon(
-                painterResource(icon),
-                contentDescription = null,
-                tint = GovUkTheme.colourScheme.textAndIcons.icon
-            )
+        ) {
+
+            if (isSelected) {
+                Box(
+                    modifier = Modifier
+                        .size(40.dp)
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.ic_selected),
+                        contentDescription = null
+                    )
+                }
+            } else {
+                Box(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .background(
+                            color = GovUkTheme.colourScheme.textAndIcons.iconSurroundPrimary,
+                            shape = CircleShape
+                        )
+                        .padding(6.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        painter = painterResource(icon),
+                        contentDescription = null,
+                        tint = GovUkTheme.colourScheme.textAndIcons.iconPrimary
+                    )
+                }
+            }
 
             MediumHorizontalSpacer()
 
+            val textColour = if (isSelected) {
+                GovUkTheme.colourScheme.textAndIcons.listSelected
+            } else {
+                GovUkTheme.colourScheme.textAndIcons.listUnselected
+            }
             BodyBoldLabel(
                 text = title,
-                modifier = Modifier.weight(1f),
-            )
-
-            MediumHorizontalSpacer()
-
-            Icon(
-                painterResource(uk.gov.govuk.design.R.drawable.ic_chevron),
-                contentDescription = null,
-                tint = GovUkTheme.colourScheme.textAndIcons.icon
+                color = textColour
             )
         }
     }
@@ -213,10 +92,8 @@ private fun TopicSelectionCardUnselectedPreview() {
         TopicSelectionCard(
             icon = R.drawable.ic_topic_benefits,
             title = "Benefits",
-            description = "Claiming benefits, managing your benefits",
             isSelected = false,
-            onClick = { },
-            modifier = Modifier.height(200.dp)
+            onClick = { }
         )
     }
 }
@@ -228,34 +105,33 @@ private fun TopicSelectionCardSelectedPreview() {
         TopicSelectionCard(
             icon = R.drawable.ic_topic_benefits,
             title = "Benefits",
-            description = "Claiming benefits, managing your benefits",
             isSelected = true,
-            onClick = { },
-            modifier = Modifier.height(200.dp)
+            onClick = { }
         )
     }
 }
 
-@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-private fun TopicVerticalCardPreview() {
+private fun TopicSelectionCardUnselectedDarkPreview() {
     GovUkTheme {
-        TopicVerticalCard(
-            icon = R.drawable.ic_topic_default,
-            title = "Title",
-            onClick = { },
-            modifier = Modifier.height(120.dp)
+        TopicSelectionCard(
+            icon = R.drawable.ic_topic_benefits,
+            title = "Benefits",
+            isSelected = false,
+            onClick = { }
         )
     }
 }
 
-@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-private fun TopicHorizontalCardPreview() {
+private fun TopicSelectionCardSelectedDarkPreview() {
     GovUkTheme {
-        TopicHorizontalCard(
-            icon = R.drawable.ic_topic_default,
-            title = "Title",
+        TopicSelectionCard(
+            icon = R.drawable.ic_topic_benefits,
+            title = "Benefits",
+            isSelected = true,
             onClick = { }
         )
     }
