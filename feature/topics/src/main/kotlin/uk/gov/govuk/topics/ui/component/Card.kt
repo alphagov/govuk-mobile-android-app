d904686d988a806b66c919f65ce5cce4ff1323e5
David Carr <david.carr@digital.cabinet-office.gov.uk>
2025-10-21 15:00:14 +0100

    GOVUKAPP-2707: Topic selection card (#482)
    * Configure talkback for focus
    
    * Remove click indication to prevent issue where cells are stuck in the clicked state

--- feature/topics/src/main/kotlin/uk/gov/govuk/topics/ui/component/Card.kt
+++ feature/topics/src/main/kotlin/uk/gov/govuk/topics/ui/component/Card.kt
@@ -4,21 +4,28 @@ import android.content.res.Configuration
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.onClick
+import androidx.compose.ui.semantics.stateDescription
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import uk.gov.govuk.design.ui.component.BodyBoldLabel
-import uk.gov.govuk.design.ui.component.GovUkCard
 import uk.gov.govuk.design.ui.component.MediumHorizontalSpacer
 import uk.gov.govuk.design.ui.theme.GovUkTheme
 import uk.gov.govuk.topics.R
@@ -31,13 +38,32 @@ fun TopicSelectionCard(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    GovUkCard(
-        modifier = modifier,
-        isSelected = isSelected,
-        onClick = { onClick() }
+    val interactionSource = remember { MutableInteractionSource() }
+
+    val backgroundColor = if (isSelected) {
+        GovUkTheme.colourScheme.surfaces.listSelected
+    } else {
+        GovUkTheme.colourScheme.surfaces.listUnselected
+    }
+
+    Card(
+        modifier = modifier
+            .clearAndSetSemantics {
+                stateDescription = "$title, ${if (isSelected) "selected" else "not selected"}"
+                onClick(label = if (isSelected) "deselect" else "Select", action = null)
+            }
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null
+            ) {
+                onClick()
+            },
+        colors = CardDefaults.cardColors(containerColor = backgroundColor)
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(GovUkTheme.spacing.medium),
             verticalAlignment = Alignment.CenterVertically
         ) {
 
