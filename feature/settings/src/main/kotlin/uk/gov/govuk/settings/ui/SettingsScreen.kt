6a205c24bbbb20267b0199c60bd886fbd49812e5
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2025-10-29 11:16:51 +0000

    Group app version number labels for accessibility

--- feature/settings/src/main/kotlin/uk/gov/govuk/settings/ui/SettingsScreen.kt
+++ feature/settings/src/main/kotlin/uk/gov/govuk/settings/ui/SettingsScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.semantics
 import androidx.core.app.NotificationManagerCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LifecycleResumeEffect
@@ -322,6 +323,7 @@ private fun AboutTheApp(
         modifier = modifier
     ) {
         CardListItem(
+            modifier = Modifier.semantics(mergeDescendants = true) { },
             isFirst = true,
             isLast = false
         ) {
