beeb4ef11e34613b61b4db659b1fc77fa4452bbb
Tim Denyer <30877434+TimGJD@users.noreply.github.com>
2025-11-18 09:47:36 +0000

    GOVUKAPP-2878 PYV item removal bug android 12 (#518)
    * Have a separate function call for the first item to any other items so the lazy column doesn't try to reuse other items as the first item when the first item is removed causing the bug.

--- feature/visited/src/main/kotlin/uk/gov/govuk/visited/ui/VisitedScreen.kt
+++ feature/visited/src/main/kotlin/uk/gov/govuk/visited/ui/VisitedScreen.kt
@@ -185,26 +185,37 @@ private fun ShowVisitedItems(
                     key = { _, item -> item.title }
                 ) { index, item ->
                     val title = item.title
-                    val lastVisited = item.lastVisited
                     val url = item.url
-
-                    ExternalLinkListItem(
-                        title = title,
-                        description = "${stringResource(R.string.visited_items_last_visited)} $lastVisited",
+                    val description = "${stringResource(R.string.visited_items_last_visited)} ${item.lastVisited}"
+                    val style = ExternalLinkListItemStyle.Button(
+                        icon = uk.gov.govuk.design.R.drawable.ic_cancel_round,
+                        altText = "${stringResource(uk.gov.govuk.design.R.string.content_desc_remove)} $title",
                         onClick = {
-                            onClick(title, url)
-                        },
-                        modifier = Modifier.animateItem(),
-                        isFirst = index == 0,
-                        isLast = index == visitedItems.size - 1,
-                        style = ExternalLinkListItemStyle.Button(
-                            icon = uk.gov.govuk.design.R.drawable.ic_cancel_round,
-                            altText = "${stringResource(uk.gov.govuk.design.R.string.content_desc_remove)} $title",
-                            onClick = {
-                                onRemoveClick(title, url)
-                            }
-                        )
+                            onRemoveClick(title, url)
+                        }
                     )
+
+                    if (index == 0) {
+                        ExternalLinkListItem(
+                            title = title,
+                            description = description,
+                            onClick = { onClick(title, url) },
+                            modifier = Modifier.animateItem(),
+                            isFirst = true,
+                            isLast = visitedItems.size == 1,
+                            style = style
+                        )
+                    } else {
+                        ExternalLinkListItem(
+                            title = title,
+                            description = description,
+                            onClick = { onClick(title, url) },
+                            modifier = Modifier.animateItem(),
+                            isFirst = false,
+                            isLast = index == visitedItems.size - 1,
+                            style = style
+                        )
+                    }
                 }
                 item {
                     LargeVerticalSpacer()
