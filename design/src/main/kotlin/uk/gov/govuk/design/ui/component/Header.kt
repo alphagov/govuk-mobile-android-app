10f77b8c871bf260ff70c431d7bda9c762bae7f9
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2025-11-25 11:00:58 +0000

    Remove unused import

--- design/src/main/kotlin/uk/gov/govuk/design/ui/component/Header.kt
+++ design/src/main/kotlin/uk/gov/govuk/design/ui/component/Header.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.hideFromAccessibility
-import androidx.compose.ui.semantics.invisibleToUser
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
