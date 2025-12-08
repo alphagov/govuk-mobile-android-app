1e54e1c36d565a6390f8ed6bc60b0f6c8f2f3a33
David Carr <david.carr@digital.cabinet-office.gov.uk>
2025-12-08 14:38:03 +0000

    Add semantics to headers

--- app/src/main/kotlin/uk/gov/govuk/login/ui/LoginSuccessScreen.kt
+++ app/src/main/kotlin/uk/gov/govuk/login/ui/LoginSuccessScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -64,6 +66,7 @@ private fun LoginSuccessScreen(
 
             LargeTitleBoldLabel(
                 text = stringResource(R.string.login_success_title),
+                modifier = Modifier.semantics { heading() },
                 color = GovUkTheme.colourScheme.textAndIcons.primary,
                 textAlign = TextAlign.Center
             )
