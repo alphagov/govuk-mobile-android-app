5f2fad177e36ca34dfa9a178ea1598ff829df9c7
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2025-12-23 15:51:09 +0000

    Removed unused imports

--- design/src/main/kotlin/uk/gov/govuk/design/ui/component/Card.kt
+++ design/src/main/kotlin/uk/gov/govuk/design/ui/component/Card.kt
@@ -6,14 +6,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
