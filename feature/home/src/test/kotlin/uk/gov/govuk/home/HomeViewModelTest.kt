b556353c33bc00a279ceffdbf8ce726f78fccd86
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-01-02 11:43:52 +0000

    Test fix

--- feature/home/src/test/kotlin/uk/gov/govuk/home/HomeViewModelTest.kt
+++ feature/home/src/test/kotlin/uk/gov/govuk/home/HomeViewModelTest.kt
@@ -4,14 +4,16 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Test
 import uk.gov.govuk.analytics.AnalyticsClient
+import uk.gov.govuk.config.data.ConfigRepo
 
 class HomeViewModelTest {
 
     private val analyticsClient = mockk<AnalyticsClient>(relaxed = true)
+    private val configRepo = mockk<ConfigRepo>(relaxed = true)
 
     @Test
     fun `Given a page view, and the user has opted into chat, then log analytics`() {
-        val viewModel = HomeViewModel(analyticsClient)
+        val viewModel = HomeViewModel(analyticsClient, configRepo)
 
         viewModel.onPageView()
 
@@ -26,7 +28,7 @@ class HomeViewModelTest {
 
     @Test
     fun `Given a page view, and the user has opted out of chat, then log analytics`() {
-        val viewModel = HomeViewModel(analyticsClient)
+        val viewModel = HomeViewModel(analyticsClient, configRepo)
 
         viewModel.onPageView()
 
