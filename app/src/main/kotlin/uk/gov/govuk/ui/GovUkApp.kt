cafb6d89ee2eeae94fec8bdf770678f48ff2d16b
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-01-05 12:59:27 +0000

    Separated firebase RC fetch and activate (after analytics consent)

--- app/src/main/kotlin/uk/gov/govuk/ui/GovUkApp.kt
+++ app/src/main/kotlin/uk/gov/govuk/ui/GovUkApp.kt
@@ -365,8 +365,8 @@ private fun GovUkNavHost(
         )
         analyticsGraph(
             analyticsConsentCompleted = {
-                // TODO GOVUKAPP-3033 activate Firebase here?
                 coroutineScope.launch {
+                    viewModel.onAnalyticsConsentCompleted()
                     appNavigation.onNext(navController)
                 }
             },
