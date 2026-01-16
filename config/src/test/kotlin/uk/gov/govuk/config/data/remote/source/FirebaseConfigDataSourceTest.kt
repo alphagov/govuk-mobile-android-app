9402a4cecf460ae59bf890c9e6e4a6e083fb5826
Lampros.Gkotsoulias <lampros.gkotsoulias@digital.cabinet-office.gov.uk>
2026-01-16 11:52:31 +0000

    More tests for Firebase config data source

--- config/src/test/kotlin/uk/gov/govuk/config/data/remote/source/FirebaseConfigDataSourceTest.kt
+++ config/src/test/kotlin/uk/gov/govuk/config/data/remote/source/FirebaseConfigDataSourceTest.kt
@@ -2,8 +2,10 @@ package uk.gov.govuk.config.data.remote.source
 
 import com.google.android.gms.tasks.Task
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import io.mockk.Ordering
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
@@ -11,8 +13,17 @@ import org.junit.Test
 
 class FirebaseConfigDataSourceTest {
 
-    private val firebaseRemoteConfig = mockk<FirebaseRemoteConfig>()
+    private val firebaseRemoteConfig = mockk<FirebaseRemoteConfig>(relaxed = true)
     private val dataSource = FirebaseConfigDataSource(firebaseRemoteConfig)
+
+    private fun <T> mockSuccessTask(resultValue: T? = null): Task<T> {
+        val task = mockk<Task<T>>()
+        every { task.isComplete } returns true
+        every { task.isCanceled } returns false
+        every { task.exception } returns null
+        every { task.result } returns resultValue
+        return task
+    }
 
     private fun <T> mockFailureTask(exception: Exception): Task<T> {
         val task = mockk<Task<T>>()
@@ -23,14 +34,34 @@ class FirebaseConfigDataSourceTest {
     }
 
     @Test
-    fun `Given fetch succeeds, when fetch is called, then return true`() = runTest {
-        val mockVoidTask = mockk<Task<Void>> {
-            every { isComplete } returns true
-            every { isCanceled } returns false
-            every { exception } returns null
-            every { result } returns null
+    fun `Given reset succeeds, when clearRemoteValues is called, then reset and re-apply settings`() =
+        runTest {
+            every { firebaseRemoteConfig.reset() } returns mockSuccessTask(null)
+
+            dataSource.clearRemoteValues()
+
+            verify(ordering = Ordering.ORDERED) {
+                firebaseRemoteConfig.reset()
+                firebaseRemoteConfig.setConfigSettingsAsync(any())
+                firebaseRemoteConfig.setDefaultsAsync(any<Int>())
+            }
         }
-        every { firebaseRemoteConfig.fetch() } returns mockVoidTask
+
+    @Test
+    fun `Given reset fails, when clearRemoteValues is called, settings are still reapplied`() = runTest {
+        every { firebaseRemoteConfig.reset() } throws Exception("Exception")
+
+        dataSource.clearRemoteValues()
+
+        verify {
+            firebaseRemoteConfig.setConfigSettingsAsync(any())
+            firebaseRemoteConfig.setDefaultsAsync(any<Int>())
+        }
+    }
+
+    @Test
+    fun `Given fetch succeeds, when fetch is called, then return true`() = runTest {
+        every { firebaseRemoteConfig.fetch() } returns mockSuccessTask(null)
 
         val result = dataSource.fetch()
         assertTrue(result)
@@ -55,13 +86,7 @@ class FirebaseConfigDataSourceTest {
 
     @Test
     fun `Given activate succeeds, when activate is called, then return true`() = runTest {
-        val mockSuccessTask = mockk<Task<Boolean>> {
-            every { isComplete } returns true
-            every { isCanceled } returns false
-            every { exception } returns null
-            every { result } returns true
-        }
-        every { firebaseRemoteConfig.activate() } returns mockSuccessTask
+        every { firebaseRemoteConfig.activate() } returns mockSuccessTask(true)
 
         val result = dataSource.activate()
         assertTrue(result)
@@ -80,6 +105,37 @@ class FirebaseConfigDataSourceTest {
         every { firebaseRemoteConfig.activate() } throws Exception("Exception")
 
         val result = dataSource.activate()
+        assertFalse(result)
+    }
+
+    @Test
+    fun `Given fetch succeeds and activate succeeds, when fetchAndActivate is called, then return true`() = runTest {
+        every { firebaseRemoteConfig.fetch() } returns mockSuccessTask(null)
+        every { firebaseRemoteConfig.activate() } returns mockSuccessTask(true)
+
+        val result = dataSource.fetchAndActivate()
+
+        assertTrue(result)
+        verify { firebaseRemoteConfig.activate() }
+    }
+
+    @Test
+    fun `Given fetch fails, when fetchAndActivate is called, then return false and do not call activate`() = runTest {
+        every { firebaseRemoteConfig.fetch() } returns mockFailureTask(Exception("Exception"))
+
+        val result = dataSource.fetchAndActivate()
+
+        assertFalse(result)
+        verify(exactly = 0) { firebaseRemoteConfig.activate() }
+    }
+
+    @Test
+    fun `Given fetch succeeds but activate fails, when fetchAndActivate is called, then return false`() = runTest {
+        every { firebaseRemoteConfig.fetch() } returns mockSuccessTask(null)
+        every { firebaseRemoteConfig.activate() } returns mockFailureTask(Exception("Activate Failed"))
+
+        val result = dataSource.fetchAndActivate()
+
         assertFalse(result)
     }
 }
